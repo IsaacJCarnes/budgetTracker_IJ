bada1f2a4b4297e36bacf77dd8437f1aa89089aa
Isaac Joshua <isaacjcarnes@gmail.com>
2022-01-29 13:55:09 -0500

    Added db.js

---
+++ public/db.js
@@ -0,0 +1,54 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({target}) => {
+  db = target.result;
+  db.createObjectStore("offlineStore", { autoIncrement: true });
+};
+
+request.onerror = function ({target}) {
+  console.log(target.errorCode);
+};
+
+request.onsuccess = (event) => {
+  if (navigator.onLine) {
+    checkDB();
+  }
+};
+
+const saveRecord = (record) => {
+  const transaction = db.transaction(["offlineStore"], "readwrite");
+  const store = transaction.objectStore("offlineStore");
+  store.add(record);
+};
+
+function checkDB() {
+  const transaction = db.transaction(["offlineStore"], "readwrite");
+  const store = transaction.objectStore("offlineStore");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then(() => {
+          const transaction = db.transaction(["offlineStore"], "readwrite");
+          const store = transaction.objectStore("offlineStore");
+
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDB);
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/favicon.ico",
+  "/db.js",
   "/styles.css",
-  "/icons/icon-144x144.png",
+  "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
